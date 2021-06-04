9b150f5665ed014e60955b874401c7042698e315
João Medeiros <joao.moreira@projuris.com.br>
2021-06-04 12:26:02 -0300

    Creating endpoint to upload large files

--- src/main/java/com/javaee/springjpamysql/controllers/v1/GarageController.java
+++ src/main/java/com/javaee/springjpamysql/controllers/v1/GarageController.java
@@ -1,21 +1,24 @@
 package com.javaee.springjpamysql.controllers.v1;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.github.pjfanning.xlsx.StreamingReader;
 import com.javaee.springjpamysql.api.v1.model.GarageDTO;
 import com.javaee.springjpamysql.services.GarageService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping(GarageController.BASE_URL)
 public class GarageController {
@@ -56,5 +59,49 @@ public class GarageController {
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Long id){
     	garageService.deleteById(id);
+    }
+
+    @PostMapping("/upload")
+    public String handleFileUpload(final HttpServletRequest request) {
+
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+        if (!isMultipart) {
+            // consider raising an error here if desired
+        }
+
+        FileItemIterator iter;
+        InputStream fileStream = null;
+        try {
+            ServletFileUpload upload = new ServletFileUpload();
+            iter = upload.getItemIterator(request);
+
+            while (iter.hasNext()) {
+                FileItemStream item = iter.next();
+                if (!item.isFormField()) {
+                    try (InputStream is = item.openStream(); Workbook workbook = StreamingReader.builder().rowCacheSize(100).bufferSize(4096).open(is)) {
+                        workbook.forEach(sheet -> sheet.forEach(row -> {
+                            if (row.getRowNum() >= 2) {
+                                String value = CellUtil.getCell(row, 1).getStringCellValue();
+                                log.info(value);
+                            }
+                        }));
+                    } catch (IOException e) {
+                        log.error(e.getMessage());
+                    }
+                }
+            }
+        } catch (FileUploadException | IOException e) {
+            e.printStackTrace();
+        }
+
+        if (fileStream != null) {
+            // a file has been sent in the http request
+            // pass the fileStream to a method on the storageService so it can be persisted
+            // note the storageService will need to be modified to receive and process the fileStream
+//            storageService.store(fileStream, name);
+        }
+
+        return "success";
     }
 }
